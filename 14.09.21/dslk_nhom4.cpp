036e6f3e7b30f7e9691b8234d40e895e4090cdf2
htan1901 <htan1901@gmail.com>
2021-09-14 13:47:57 +0700

    add head add tail

--- 14.09.21/dslk_nhom4.cpp
+++ 14.09.21/dslk_nhom4.cpp
@@ -15,9 +15,35 @@ void initList(List *list) {
 	list->tail = NULL;
 }
 
+bool isEmpty(List list) {
+	return list.head == NULL;
+}
+
 Node* createNode(int value) {
 	Node* p = new Node;
 	p->data = value;
 	p->next = NULL;
 	return p;
+}
+
+void insertFront(List &list, Node *node) {
+	if(isEmpty(list)) {
+		list.head = node;
+		list.tail = node;
+		return;
+	}
+	node->next = list.head;
+	list.head = node;
+	return;
+}
+
+void insertTail(List &list, Node *node) {
+	if(isEmpty(list)) {
+		list.head = node;
+		list.tail = node;
+		return;
+	}
+	list.tail->next = node;
+	list.tail = node;
+	return;
 }

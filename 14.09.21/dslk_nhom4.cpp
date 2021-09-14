50fa74264c06c393783067d67d4ec14019883721
htan1901 <htan1901@gmail.com>
2021-09-14 13:35:33 +0700

    add function create node

--- 14.09.21/dslk_nhom4.cpp
+++ 14.09.21/dslk_nhom4.cpp
@@ -11,5 +11,13 @@ struct List {
 };
 
 void initList(List *list) {
-	
+	list->head = NULL;
+	list->tail = NULL;
+}
+
+Node* createNode(int value) {
+	Node* p = new Node;
+	p->data = value;
+	p->next = NULL;
+	return p;
 }

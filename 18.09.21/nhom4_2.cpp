c32b5bfa75427f8b97386bdef9908d51271e040e
htan1901 <htan1901@gmail.com>
2021-09-18 13:53:06 +0700

    hello

---
+++ 18.09.21/nhom4_2.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct SinhVien {
+	char MaSo[10];
+	char HoTen[30];
+	float DiemTB;
+};
+
+struct Node {
+	SinhVien info;
+	Node* next;
+};
+
+struct List {
+	Node* head;
+	Node* tail;
+};
+
+void initList(List &list) {
+
+}
+
+void xuatList(List list) {
+
+}
+
+void xuatListDTB(List list) {
+
+}
+
+Node* searchSV(List list, char* ten) {
+
+}
+
+void swapData(Node *a, Node *b) {
+
+}
+
+void sort(List &list) {
+
+}
+
+int main() {
+	
+}
+
+
+

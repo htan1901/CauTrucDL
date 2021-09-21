6a449fccc002a90cda71901e6eb00edb0686aa5f
htan1901 <htan1901@gmail.com>
2021-09-21 13:05:34 +0700

    commitee

---
+++ 21.09.21/nhom4.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct Node {
+	int info;
+	Node* next;
+};
+typedef Node* pList;
+
+void initList(pList &list) {
+	list = NULL;
+}
+
+
+
+int main() {
+
+}

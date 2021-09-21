83ed39e903a8682f08ab9465da308b4acdfdb3b4
htan1901 <htan1901@gmail.com>
2021-09-21 13:35:05 +0700

    add funcs

--- 21.09.21/nhom4.cpp
+++ 21.09.21/nhom4.cpp
@@ -8,8 +8,38 @@ struct Node {
 };
 typedef Node* Stack;
 
+Node* createNode(int value) {
+	Node* p = new Node;
+	p->info = value;
+	p->next = NULL;
+	return p;
+}
+
+bool isEmpty(Stack stack) {
+	return stack == NULL;
+}
+
 void initStack(Stack &stack){
 	stack = NULL;
+}
+
+void push(Stack &stack, int x) {
+	Node *a = createNode(x);
+	if(isEmpty(stack)) {
+		stack = a;
+		return;
+	}
+	a->next = stack;
+	stack = a;
+	return;
+}
+
+void pop(Stack &stack) {
+	if(isEmpty(stack))
+		return;
+	Node* a = stack;
+	stack = stack->next;
+	free(a);
 }
 
 int main() {
